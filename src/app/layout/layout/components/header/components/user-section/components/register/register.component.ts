7aad71ef37fa4843b575516f292f4b25a13afd02
Bojan Maksimovic <bojanm245@gmail.com>
2022-06-23 21:05:19 +0200

    Added the register functionality

--- src/app/layout/layout/components/header/components/user-section/components/register/register.component.ts
+++ src/app/layout/layout/components/header/components/user-section/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { SpinnerFunctions } from 'src/app/shared/classes/spinner-functions';
+import { RegisterFormService } from '../../services/forms/register-form.service';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  errorMessage: string = "";
+  successMessage: string = "";
+
+  constructor(
+    public registerFormService: RegisterFormService,
+    private dialogRef: MatDialogRef<RegisterComponent>,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    this.errorMessage = "";
+    this.successMessage = "";
+    this.registerFormService.initializeForm();
+  }
+
+  submit(): void {
+    SpinnerFunctions.showSpinner();
+
+    this.registerFormService.submitForm().subscribe({
+      next: () => {
+        SpinnerFunctions.hideSpinner();
+        this.registerFormService.initializeForm();
+        this.registerFormService.buttonIsDisabled = false;
+        this.successMessage = "Your account was created successfully.";
+        this.errorMessage = "";
+      },
+      error: (err) => {
+        SpinnerFunctions.hideSpinner();
+        this.registerFormService.buttonIsDisabled = false;
+        
+        this.successMessage = "";
+        
+        switch(err.status) {
+          case 422:
+            this.errorMessage = err.message;
+            break;
+          case 500:
+            this.errorMessage = "We encountered an error. Please try again later.";
+            break;
+          default:
+            this.errorMessage = "";
+        }
+      }
+    });
+  }
+
+  redirectToLogin(): void {
+    this.dialogRef.close();
+    this.dialog.open(LoginComponent, {
+      width: 'auto'
+    });
   }
 
 }
--- src/app/layout/layout/components/header/components/user-section/services/forms/register-form.service.ts
+++ src/app/layout/layout/components/header/components/user-section/services/forms/register-form.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { BaseFormService } from 'src/app/shared/services/forms/base-form.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RegisterFormService {
+export class RegisterFormService extends BaseFormService {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) {
+    super();
+  }
+
+  initializeForm(): void {
+    this.form = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.pattern(/^(?=[a-zA-Z0-9\._]{3,12}$)(?!.*[_.]{2})[^_.].*[^_.]$/)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\@\$\!\%\*\?\&\#])[A-Za-z\d\@\$\!\%\*\?\&]{8,}$/)])
+    });
+  }
+
+  submitForm(): Observable<any> {
+    this.buttonIsDisabled = true;
+    return this.authService.register(this.form.get('email').value, this.form.get('password').value, this.form.get('username').value);
+  }
 }
