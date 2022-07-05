3c90c2358fd019c92e0a1d6421903e5390af57e1
unknown <bojanm245@gmail.com>
2022-07-05 22:16:46 +0200

    Fixed the bug where orders couldn't be filtered by date on the admin panel

--- src/app/cart/cart/services/order.service.ts
+++ src/app/cart/cart/services/order.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { IOrderConstants } from 'src/app/menu/menu/interfaces/i-order-constants';
 import { API } from 'src/app/shared/constants/apis';
 import { CONFIG } from 'src/app/shared/constants/config';
+import { IBasePagedDateSearch, IPagedSearch } from 'src/app/shared/interfaces/i-base-search';
+import { IPagedResponse } from 'src/app/shared/interfaces/i-paged-response';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { IOrder } from '../interfaces/i-order';
 
@@ -20,5 +22,21 @@ export class OrderService extends ApiService<IOrder> {
 
   constants(): Observable<IOrderConstants> {
     return this.http.get<IOrderConstants>(CONFIG.SERVER + API.orders + "/constants");
+  }
+
+  override getAllPaged(search: IPagedSearch): Observable<IPagedResponse<IOrder>> {
+    const basePagedDateSearch: IBasePagedDateSearch = search as IBasePagedDateSearch;
+
+    let queryString = `?page=${basePagedDateSearch.page}&perPage=${basePagedDateSearch.perPage}&keyword=${basePagedDateSearch.keyword}`;
+
+    if(basePagedDateSearch.dateFrom) {
+      queryString += `&dateFrom=${basePagedDateSearch.dateFrom}`;
+    }
+
+    if(basePagedDateSearch.dateTo) {
+      queryString += `&dateTo=${basePagedDateSearch.dateTo}`;
+    }
+
+    return this.http.get<IPagedResponse<IOrder>>(this.apiPrefix + this.apiPath + queryString);
   }
 }
